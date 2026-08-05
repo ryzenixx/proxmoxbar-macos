0a7e0a694be90fcf21d73ca2eb21c088ebd8f5b5
ryzenixx <mael.duret@icloud.com>
2026-08-05 10:54:31 +0200

    feat: init project

---
+++ ProxmoxBar/ProxmoxBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxmoxBarApp.swift
+//  ProxmoxBar
+//
+//  Created by Mael DURET on 05/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProxmoxBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProxmoxBarTests/ProxmoxBarTests.swift
@@ -0,0 +1,19 @@
+//
+//  ProxmoxBarTests.swift
+//  ProxmoxBarTests
+//
+//  Created by Mael DURET on 05/08/2026.
+//
+
+import Testing
+@testable import ProxmoxBar
+
+struct ProxmoxBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
